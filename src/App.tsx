e58cce809501a6d615e9dd2701163b61ecafca79
stipochka <drax23885@gmail.com>
2025-02-01 20:36:31 +0300

    Added header, created home and NotFound pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Header } from './components/Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { NotFound } from './pages/NotFound'
 import './App.css'
 
 function App() {
 	return (
-		<div>
+		<BrowserRouter>
 			<Header />
-		</div>
+			<main>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='*' element={<NotFound />} />
+				</Routes>
+			</main>
+		</BrowserRouter>
 	)
 }
 
